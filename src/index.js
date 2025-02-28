2e5422fb65282831121fd8e587079061aa668517
muhammadzaidathar@gmail.com <muhammadzaidathar@gmail.com>
2025-02-28 18:27:34 +0500

    Add new files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import app from './app.js';
 
 
 
@@ -8,4 +9,16 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+     .then(() => {
+          app.listen(process.env.PORT || 8000, () => {
+               console.log(`Server is running on Port ${process.env.PORT}`);
+
+          })
+     }
+
+     )
+     .catch((err) => {
+          console.log("MONGODB connection failed !! ", err);
+
+     })
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,12 @@
+class ApiResponse {
+     constructor(
+          message = "Success",
+          statusCode,
+          data
+     ){
+          this.message = message
+          this.statusCode = statusCode
+          this.data = data
+          this.success = statusCode < 400
+     }
+}
